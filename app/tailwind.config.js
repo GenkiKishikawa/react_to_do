eef741f258bfbe341112c25cd09dd5586b8542ff
KishikawaGenki <kishikawa@vic.co.jp>
2024-04-03 14:22:05 +0900

    Setup tailwindcss and apply it to related files

---
+++ app/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./src/**/*.tsx",
+    "./src/*.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
